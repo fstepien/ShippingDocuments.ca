5511099678f6e65a93be15e393f295a8c833bee7
Filip Stepien <contact@filipstepien.com>
2018-03-26 19:10:36 -0400

    update add product and product list components

--- src/AddProduct.js
+++ src/AddProduct.js
@@ -4,7 +4,7 @@ class AddProduct extends React.Component {
 
 descRef = React.createRef();
 unRef = React.createRef();
-classRef = React.createRef();
+classDGRef = React.createRef();
 pgRef = React.createRef();
 nosRef = React.createRef();
 kgRef = React.createRef();
@@ -15,7 +15,7 @@ addProduct = (e) => {
     const product = {
       desc: this.descRef.value.value,
       price: this.unRef.value.value, 
-      class: this.classRef.value.value, 
+      classDG: this.classDGRef.value.value, 
       pg: this.pgRef.value.value,
       nos: this.nosRef.value.value,
       kg: this.kgRef.value.value
@@ -27,10 +27,11 @@ addProduct = (e) => {
 render() {
 return (
   <div className="box">
+    <span className="add-product-label">My Products</span>
     <form className="product-edit" onSubmit={this.addProduct}>
       <input name="desc" ref={this.descRef} type="text" placeholder="Product Name" required />
       <input name="un" ref={this.unRef} type="text" placeholder="UN Number" />
-      <input name="class" ref={this.classRef} type="text" placeholder="Class" />
+      <input name="class" ref={this.classDGRef} type="text" placeholder="Class" />
       <input name="pg" ref={this.pgRef} type="text" placeholder="P.G." />
       <input name="kg" ref={this.kgRef} type="text" placeholder="Weight (kg)" />
       <input name="nos" ref={this.nosRef} type="text" placeholder="N.O.S." />
--- src/ProductList.js
+++ src/ProductList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Product from './Product';
+
 
 class ProductList extends React.Component {
 
@@ -7,10 +7,13 @@ class ProductList extends React.Component {
 render() {
 return (
   <div className="box">
+    <span className="my-product-label">My Products</span>
     <ul className="product-list">
-      {Object.keys(this.props.products).map(key => 
-      <li key={key}>{this.props.products.desc}</li>
-      )}
+      {Object.keys(this.props.products)
+      .sort((a,b) => this.props.products[a].desc.toLowerCase() > this.props.products[b].desc.toLowerCase() ? 1: -1)
+      .map(key => (
+          <li key={key} className="product-item">{this.props.products[key].desc}</li>
+      ))}
     </ul>
   </div>
        
