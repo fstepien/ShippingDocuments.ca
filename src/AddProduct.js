fd29d10378474b743a3c86359a0933b419cc9253
Filip Stepien <contact@filipstepien.com>
2018-04-11 22:00:54 -0400

    update pg to be #777 while value=0 (to match placeholder

--- src/AddProduct.js
+++ src/AddProduct.js
@@ -1,54 +1,102 @@
-import React from 'react';
+import React from "react";
 
 class AddProduct extends React.Component {
+  descRef = React.createRef();
+  unRef = React.createRef();
+  classDGRef = React.createRef();
+  pgRef = React.createRef();
+  nosRef = React.createRef();
+  kgRef = React.createRef();
 
-descRef = React.createRef();
-unRef = React.createRef();
-classDGRef = React.createRef();
-pgRef = React.createRef();
-nosRef = React.createRef();
-kgRef = React.createRef();
-
-//Property instead of a method
-addProduct = (e) => {
+  state = {
+    pgValue: false
+  };
+  //Property instead of a method
+  addProduct = e => {
     e.preventDefault();
     const product = {
       desc: this.descRef.value.value,
-      un: this.unRef.value.value, 
-      classDG: this.classDGRef.value.value, 
+      un: this.unRef.value.value,
+      classDG: this.classDGRef.value.value,
       pg: this.pgRef.value.value,
       nos: this.nosRef.value.value,
       kg: this.kgRef.value.value
-    }
+    };
     this.props.addProduct(product);
     e.currentTarget.reset();
-}
+  };
 
-render() {
-if(this.pgRef){
-  console.log('empty');
-} else {
-  console.log(`not empty ${this.pgRef}`)
-}
-return (
-  <div className="box">
-    <span className="add-product-label">Add Products</span>
-    <form className="product-edit" onSubmit={this.addProduct}>
-      <input name="desc" ref={this.descRef} type="text" placeholder="Product Name" required />
-      <input name="un" ref={this.unRef} type="text" placeholder="UN Number" pattern="[0-9]{4}" title="UN numbers are four-digit numbers"/>
-      <input name="class" ref={this.classDGRef} type="text" placeholder="Class" />
-      <select name="pg" ref={this.pgRef} type="text" placeholder="P.G."> 
-        <option defaultValue value="">P.G.</option>
-        <option>I</option>
-        <option>II</option>
-        <option>III</option>
-      </select>
-      <input name="kg" ref={this.kgRef} type="text" placeholder="Weight (kg)" required/>
-      <input name="nos" ref={this.nosRef} type="text" placeholder="N.O.S." />
-      <button type="submit">+ Add Product</button>
-    </form>
-  </div>
-       
+  pgUpdateColor = e => {
+    e.preventDefault();
+    if (e.currentTarget.value === "") {
+      this.setState({ pgValue: false });
+    } else {
+      this.setState({ pgValue: true });
+    }
+  };
+  render() {
+    let pgColor;
+    if (!this.state.pgValue) {
+      pgColor = { color: "#777" };
+    } else {
+      pgColor = { color: "black" };
+    }
+    return (
+      <div className="box">
+        <span className="add-product-label">Add Products</span>
+        <form className="product-edit" onSubmit={this.addProduct}>
+          <input
+            name="desc"
+            ref={this.descRef}
+            type="text"
+            placeholder="Product Name"
+            required
+          />
+          <input
+            name="un"
+            ref={this.unRef}
+            type="text"
+            placeholder="UN Number"
+            pattern="[0-9]{4}"
+            title="UN numbers are four-digit numbers"
+          />
+          <input
+            name="class"
+            ref={this.classDGRef}
+            type="text"
+            placeholder="Class"
+          />
+          <select
+            name="pg"
+            ref={this.pgRef}
+            type="text"
+            placeholder="P.G."
+            onChange={this.pgUpdateColor}
+            style={pgColor}
+          >
+            <option defaultValue value="">
+              P.G.
+            </option>
+            <option>I</option>
+            <option>II</option>
+            <option>III</option>
+          </select>
+          <input
+            name="kg"
+            ref={this.kgRef}
+            type="text"
+            placeholder="Weight (kg)"
+            required
+          />
+          <input
+            name="nos"
+            ref={this.nosRef}
+            type="text"
+            placeholder="N.O.S."
+          />
+          <button type="submit">+ Add Product</button>
+        </form>
+      </div>
     );
   }
 }
