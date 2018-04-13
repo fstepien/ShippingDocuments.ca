dd80785ef786ec28c4a3ff5c046f8642ad1b62aa
Filip Stepien <contact@filipstepien.com>
2018-04-13 12:35:59 -0400

    update tdg training link

--- src/Footer.js
+++ src/Footer.js
@@ -1,54 +1,85 @@
-import React from 'react';
-import Terms from './Terms';
-import Privacy from './Privacy';
+import React from "react";
+import Terms from "./Terms";
+import Privacy from "./Privacy";
 
 class Footer extends React.Component {
-
-state = {
+  state = {
     termsDisplay: false,
     privacyDisplay: false,
     bottomDisplay: true
-  }
+  };
 
-toggleTerms = () => {
-  this.setState({termsDisplay: !this.state.termsDisplay})
-  let bottomDisplay = {...this.state.bottomDisplay};
-  bottomDisplay = true;
-  this.setState({bottomDisplay});
-}  
-togglePrivacy = () => {
-  this.setState({privacyDisplay: !this.state.privacyDisplay})
-} 
-showTermsOnly = () => {
-  
-  let bottomDisplay = {...this.state.bottomDisplay};
-  bottomDisplay = false;
-  this.setState({bottomDisplay});
-  this.setState({termsDisplay: !this.state.termsDisplay})
-}  
+  toggleTerms = () => {
+    this.setState({ termsDisplay: !this.state.termsDisplay });
+    let bottomDisplay = { ...this.state.bottomDisplay };
+    bottomDisplay = true;
+    this.setState({ bottomDisplay });
+  };
+  togglePrivacy = () => {
+    this.setState({ privacyDisplay: !this.state.privacyDisplay });
+  };
+  showTermsOnly = () => {
+    let bottomDisplay = { ...this.state.bottomDisplay };
+    bottomDisplay = false;
+    this.setState({ bottomDisplay });
+    this.setState({ termsDisplay: !this.state.termsDisplay });
+  };
 
-render() {     
-
-return (
-
-
-    <footer>
+  render() {
+    return (
+      <footer>
         <div className="footer container mt50">
           <ul>
-            <li></li><a href="mailto:contact@shippingdocuments.ca" target="_blank" rel="noopener noreferrer">Contact Us</a>
-            {/* <li><a href="#">TDG Training</a></li> */}
-            <li><a href="https://www.tc.gc.ca/eng/tdg/page-1288.html" rel="noopener noreferrer" target="_blank">TDG Regulations</a></li>
-            <li><a onClick={this.showTermsOnly}>Terms</a> and <a onClick={this.togglePrivacy}>Privacy Policy</a></li>
+            <li />
+            <a
+              href="mailto:contact@shippingdocuments.ca"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Contact Us
+            </a>
+            <li>
+              <a
+                href="https://worksitesafety.ca/training/online/tdg-online-training/"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                TDG Training
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.tc.gc.ca/eng/tdg/page-1288.html"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                TDG Regulations
+              </a>
+            </li>
+            <li>
+              <a onClick={this.showTermsOnly}>Terms</a> and{" "}
+              <a onClick={this.togglePrivacy}>Privacy Policy</a>
+            </li>
 
             <li>&#169; ShippingDocuments.ca 2018</li>
           </ul>
         </div>
-        <Terms termsDisplay={this.state.termsDisplay} toggleTerms={this.toggleTerms} bottomDisplay={this.state.bottomDisplay}/>
-        {this.state.privacyDisplay ?
-        <Privacy privacyDisplay={this.state.privacyDisplay} togglePrivacy={this.togglePrivacy}/> : ""}
-  </footer>
-      )   
-    }
+        <Terms
+          termsDisplay={this.state.termsDisplay}
+          toggleTerms={this.toggleTerms}
+          bottomDisplay={this.state.bottomDisplay}
+        />
+        {this.state.privacyDisplay ? (
+          <Privacy
+            privacyDisplay={this.state.privacyDisplay}
+            togglePrivacy={this.togglePrivacy}
+          />
+        ) : (
+          ""
+        )}
+      </footer>
+    );
+  }
 }
 
 export default Footer;
--- src/Header.js
+++ src/Header.js
@@ -1,23 +1,32 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Header = (props) => {
-    return (
-        <header>
-          <div className="container">
-          <Link to={`/`}>
-            <h1 className="logo">
-                <span className="tdg">TDG</span>
-                <span className="bol">BOL</span>
-            </h1>
-            </Link>
-            {/* <ul>    
-                <li><button>TDG Training</button></li>
-            </ul> */}
-          </div>
-        </header>
-        
-    );
-}
+const Header = props => {
+  return (
+    <header>
+      <div className="container">
+        <Link to={`/`}>
+          <h1 className="logo">
+            <span className="tdg">TDG</span>
+            <span className="bol">BOL</span>
+          </h1>
+        </Link>
+        <ul>
+          <li>
+            <button>
+              <a
+                href="https://worksitesafety.ca/training/online/tdg-online-training/"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                TDG Training
+              </a>
+            </button>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
